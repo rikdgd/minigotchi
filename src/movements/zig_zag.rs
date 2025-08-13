dc9adc2fc99e48d8a64af1de62acf6849520d278
Rik de Goede <rik.dgd@gmail.com>
2025-08-14 01:36:38 +0200

    Implement ZigZag movement structure and timer for state updates

--- src/movements/zig_zag.rs
+++ src/movements/zig_zag.rs
@@ -1,13 +1,58 @@
 use crate::movements::CreatureMovement;
 use crate::utils::Location;
+use macroquad::time::get_frame_time;
 
+/// This is the sprite size for the sprite used in the `ZigZag` movement, which is 
+/// 15x15 pixels.
+const SPRITE_SIZE: f32 = 15.0;
+
+/// The movement that should be displayed when the creature is in the **"Kid"** growth
+/// stage.
 pub struct ZigZag {
     base_location: Location,
+    x_shift: f32,
+    y_shift: f32,
+    x_toggle: bool,
+    y_toggle: bool,
     timer: f32,
+}
+
+impl ZigZag {
+    pub fn base_location(mut self, location: Location) -> Self {
+        self.base_location = location;
+        self
+    }
+}
+
+impl Default for ZigZag {
+    fn default() -> Self {
+        let center_screen = Location {
+            x: 100.0, 
+            y: 100.0,
+        };
+        
+        Self {
+            base_location: center_screen.translate(
+                SPRITE_SIZE / 2.0, 
+                SPRITE_SIZE / 2.0
+            ),
+            x_shift: 0.0,
+            y_shift: 0.0,
+            x_toggle: true,
+            y_toggle: true,
+            timer: 0.0,
+        }
+    }
 }
 
 impl CreatureMovement for ZigZag {
     fn next_position(&mut self) -> Option<Location> {
+        self.timer += get_frame_time();
+        if self.timer > 1.0 {
+            // TODO: update state
+            self.timer = 0.0;
+        }
+        
         todo!()
     }
 }
