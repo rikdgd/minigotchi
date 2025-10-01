5aff75cd7ea0ae1189cbeaea27991f089234bcd0
Rik de Goede <rik.dgd@gmail.com>
2025-10-01 23:40:44 +0200

    remove unused import

--- src/animations/creature_eat.rs
+++ src/animations/creature_eat.rs
@@ -1,6 +1,6 @@
 use macroquad::prelude::*;
 use super::{Animation, PopupAnimation};
-use crate::{include_texture, SCREEN_HEIGHT, SCREEN_WIDTH};
+use crate::include_texture;
 
 
 pub struct CreatureEatAnimation {
@@ -11,13 +11,13 @@ pub struct CreatureEatAnimation {
 impl Animation for CreatureEatAnimation {
     async fn render(&mut self) {
         let draw_location = self.frame_draw_location();
-        
+
         loop {
             if self.frame_timer > 0.5 {
                 self.current_frame += 1;
                 self.frame_timer = 0.0;
             }
-            
+
             draw_texture(
                 &Self::frames()[self.current_frame],
                 draw_location.x,
