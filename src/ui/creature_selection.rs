213517718d8b7c4720b989d7c1b8047082661f8a
Rik de Goede <rik.dgd@gmail.com>
2026-07-21 23:18:19 +0200

    Add a confirm button and let the render method return the selected shape when after render loop.

--- src/ui/creature_selection.rs
+++ src/ui/creature_selection.rs
@@ -1,28 +1,60 @@
 use macroquad::prelude::*;
-use serde::Serialize;
-use crate::creature::Creature;
-use crate::game_state::GameState;
 use crate::shapes::CreatureShapes;
 use crate::ui::button::Button;
 
 
+/// The **CreatureSelection** struct manages the state of the creature selection screen
+/// when creating a new save. Whenever it is drawn to the screen it will automatically update its
+/// state.
+///
+/// ## Fields:
+/// * `selected_shape` - The shape that is currently selected in the in-game menu.
+/// * `next_btn` - The button component that the user can use to select the next shape.
+/// * `confirm_btn` - The button component that the user can use to select the next shape.
 #[derive(Debug, Clone)]
 pub struct CreatureSelection {
     selected_shape: CreatureShapes,
     next_btn: Button,
+    confirm_btn: Button,
 }
 
 impl CreatureSelection {
-    pub fn render(&mut self) {
-        self.next_btn.render();
+    /// Renders and updates the state of the creature selection screen.
+    pub fn render(&mut self) -> CreatureShapes {
+        let creature_shape: CreatureShapes;
+        loop {
+            self.next_btn.render();
+            self.confirm_btn.render();
 
-        self.update();
+            let creature_texture = self.selected_shape.get_texture();
+            draw_texture_ex(
+                &creature_texture,
+                50.0,
+                50.0,
+                BLACK,
+                DrawTextureParams::default(),
+            );
+
+            // Update the menu's state and when the user picked a shape, break the render loop.
+            if let Some(shape) = self.update() {
+                creature_shape = shape;
+                break;
+            }
+        }
+
+        creature_shape
     }
 
-    fn update(&mut self) {
+    fn update(&mut self) -> Option<CreatureShapes> {
         if self.next_btn.is_clicked() {
             self.selected_shape = Self::next_creature(self.selected_shape);
         }
+
+        if self.confirm_btn.is_clicked() {
+            return Some(self.selected_shape);
+        }
+
+        None
     }
 
     fn next_creature(creature: CreatureShapes) -> CreatureShapes {
@@ -40,13 +72,18 @@ impl CreatureSelection {
 
 impl Default for CreatureSelection {
     fn default() -> Self {
-        let mut btn = Button::default();
-        btn.text = "->".to_string();
-        btn.pos = (100.0, 100.0).into();
+        let mut next_btn = Button::default();
+        next_btn.text = "next".to_string();
+        next_btn.pos = (100.0, 100.0).into();
+
+        let mut confirm_btn = Button::default();
+        confirm_btn.text = "confirm".to_string();
+        confirm_btn.pos = (100.0, 140.0).into();
 
         Self {
             selected_shape: CreatureShapes::Turtle,
-            next_btn: btn,
+            next_btn,
+            confirm_btn,
         }
     }
 }
