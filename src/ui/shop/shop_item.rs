c2308f17590099143ba4794da314fcbc4f7f4416
Rik de Goede <rik.dgd@gmail.com>
2026-07-30 21:04:11 +0200

    Update the positions of elements in the ShopItem component.

--- src/ui/shop/shop_item.rs
+++ src/ui/shop/shop_item.rs
@@ -6,6 +6,9 @@ use crate::items::inventory::Inventory;
 use crate::items::creature_color::CreatureColor;
 
 
+// The sprites for items in the shop are always 15x15 pixels
+const ITEM_SPRITE_DIMENSION: f32 = 15.0;
+
 /// The `ShopItem` structure can be used to render a buyable item in the shop menu.
 /// It manages its own state and can be rendered using the `render()` function.
 ///
@@ -21,9 +24,18 @@ pub struct ShopItem {
 
 impl ShopItem {
     const ITEM_WIDTH: f32 = (SCREEN_WIDTH as f32 * 0.9).round();
-    const ITEM_HEIGHT: f32 = 20.0;
+    const ITEM_HEIGHT: f32 = 30.0;
     const X_LOCATION: f32 = 10.0;
+    const ITEM_NAME_FONT_SIZE: f32 = 16.0;
+    const PRICE_FONT_SIZE: f32 = 14.0;
     
+    /// Returns a new `ShopItem` instance.
+    /// 
+    /// ## Parameters:
+    /// * `item` - The item that can be bought using the new ShopItem.
+    /// * `sprite` - The sprite that should be rendered in the new ShopItem.
+    /// * `item_index` - The index for this item in the list of all shop items. This is used
+    ///   to determine at what height the ShopItem should be rendered.
     pub fn new(item: Box<dyn BuyableItem>, sprite: Texture2D, item_index: u32) -> Self {
         Self {
             item,
@@ -52,26 +64,28 @@ impl ShopItem {
 
         draw_texture_ex(
             &self.sprite,
-            self.area.x + 2.0,
-            self.area.y + 2.0,
+            self.area.x + 8.0,
+            (self.area.y + (self.area.h - ITEM_SPRITE_DIMENSION) / 2.0).round(),
             BLACK,
             DrawTextureParams::default(),
         );
 
         draw_text(
             self.item.name(),
-            self.area.x + 10.0,
-            self.area.y + 2.0,
-            16.0,
+            self.area.x + 16.0 + ITEM_SPRITE_DIMENSION,
+            self.area.y + (self.area.h + Self::ITEM_NAME_FONT_SIZE / 2.0) / 2.0,
+            Self::ITEM_NAME_FONT_SIZE,
             BLACK,
         );
 
+        let price_txt = format!("{}$", self.item.price());
+        let price_dimensions = measure_text(&price_txt, None, Self::PRICE_FONT_SIZE as u16, 1.0);
         draw_text(
-            &format!("price: {}", self.item.price()),
-            self.area.x + self.area.w - 10.0,
-            self.area.y + 10.0,
-            14.0,
-            Color { r: 0.85, g: 0.85, b: 0.85, a: 1.0 },
+            &price_txt,
+            self.area.x + self.area.w - price_dimensions.width - 8.0,
+            (self.area.y + (self.area.h + Self::PRICE_FONT_SIZE / 2.0) / 2.0).round(),
+            Self::PRICE_FONT_SIZE,
+            Color { r: 0.2, g: 0.2, b: 0.2, a: 1.0 },
         );
     }
 }
