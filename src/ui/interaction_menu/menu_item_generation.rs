3b80bfce4402306df9106897fd2903f93cf865ee
Rik de Goede <rik.dgd@gmail.com>
2026-08-27 17:21:53 +0200

    Use the `set_item_heights` function for food item generation.

--- src/ui/interaction_menu/menu_item_generation.rs
+++ src/ui/interaction_menu/menu_item_generation.rs
@@ -3,26 +3,24 @@ use crate::food::Food;
 use crate::creature_game::CreatureGame;
 
 
-const BASE_ITEM_HIGHT: f32 = ITEM_CONTAINER_AREA.y + 10.0;
+const BASE_ITEM_HEIGHT: f32 = ITEM_CONTAINER_AREA.y + 10.0;
 
 /// Generates and returns all the **InteractionMenuItem** that should be displayed in the
 /// **InteractionMenu** when feeding the creature. This function makes sure all items have the
 /// correct location on the screen, and that the correct food items are present and in the right
 /// order.
 pub fn gen_all_food_items() -> [InteractionMenuItem<Food>; 3] {
-    let mut items = [
+    set_item_heights([
         InteractionMenuItem::new(Food::Soup),
         InteractionMenuItem::new(Food::Cookie),
         InteractionMenuItem::new(Food::Burger),
-    ];
-
-    for (i, item) in items.iter_mut().enumerate() {
-        item.area.y = i as f32 * (InteractionMenuItem::<Food>::DIMENSIONS.height + 5.0) + BASE_ITEM_HIGHT;
-    }
-
-    items
+    ])
 }
 
+/// Generates and returns all the **InteractionMenuItem** that should be displayed in the
+/// **InteractionMenu** when playing with the creature. This function makes sure all items have the
+/// correct location on the screen, and that the correct items are present and in the right
+/// order.
 pub fn gen_all_game_items() -> [InteractionMenuItem<CreatureGame>; 3] {
     set_item_heights([
         InteractionMenuItem::new(CreatureGame::Drawing),
@@ -31,9 +29,12 @@ pub fn gen_all_game_items() -> [InteractionMenuItem<CreatureGame>; 3] {
     ])
 }
 
+/// Takes ownership of an array containing `InteractionMenuItem`s and automatically sets their
+/// drawing height based on their order in the array. The array with the modified
+/// `InteractionMenuItem`s is then returned again.
 fn set_item_heights<T: CreatureInteraction>(mut items: [InteractionMenuItem<T>; 3]) -> [InteractionMenuItem<T>; 3] {
     for (i, item) in items.iter_mut().enumerate() {
-        item.area.y = i as f32 * (InteractionMenuItem::<Food>::DIMENSIONS.height + 5.0) + BASE_ITEM_HIGHT;
+        item.area.y = i as f32 * (InteractionMenuItem::<T>::DIMENSIONS.height + 5.0) + BASE_ITEM_HEIGHT;
     }
     
     items
