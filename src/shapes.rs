87093081a7f8b4e9bf224eebaaecc511dd33c4a1
Rik de Goede <rik.dgd@gmail.com>
2026-08-31 17:40:55 +0200

    Revert backdrops to be 25x25 pixels again, simplifying the drawing process.

--- src/shapes.rs
+++ src/shapes.rs
@@ -24,8 +24,6 @@ pub enum CreatureShapes {
 }
 
 impl CreatureShapes {
-    pub const TEXTURE_DIMENSION: f32 = 25.0;
-    pub const BACKDROP_DIMENSION: f32 = 27.0;
     
     pub fn get_texture(&self) -> Texture2D {
         match self {
