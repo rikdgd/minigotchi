107f0a80dfec41492ff119974f14b8465391b2dc
Rik de Goede <rik.dgd@gmail.com>
2026-09-10 13:55:14 +0200

    Add an application icon

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ mod items;
 mod game_runner;
 mod creature_personality;
 mod coin_drops;
+mod favicon;
 
 use macroquad::miniquad::conf::Icon;
 use macroquad::prelude::*;
@@ -32,38 +33,18 @@ async fn main() {
 }
 
 fn main_window_conf() -> Conf {
+    let icon = Icon {
+        small: favicon::SMALL_ICON,
+        medium: [0; 4096],
+        big: [0; 16384],
+    };
+    
     Conf {
         window_title: String::from("minigotchi"),
         window_width: SCREEN_WIDTH,
         window_height: SCREEN_HEIGHT,
         window_resizable: false,
-        icon: Some(get_app_icon()),
+        icon: Some(icon),
         ..Default::default()
-    }
-}
-
-fn get_app_icon() -> Icon {
-    let small_bytes = include_bytes!("../resources/icon/icon_16x16.png");
-    let mut small_buffer = [0u8; 1024];
-    for (i, byte) in small_bytes.iter().enumerate() {
-        small_buffer[i] = byte.to_owned();
-    }
-
-    let medium_bytes = include_bytes!("../resources/icon/icon_32x32.png");
-    let mut medium_buffer = [0u8; 4096];
-    for (i, byte) in medium_bytes.iter().enumerate() {
-        medium_buffer[i] = byte.to_owned();
-    }
-
-    let big_bytes = include_bytes!("../resources/icon/icon_64x64.png");
-    let mut big_buffer = [0u8; 16384];
-    for (i, byte) in big_bytes.iter().enumerate() {
-        big_buffer[i] = byte.to_owned();
-    }
-    
-    Icon {
-        small: small_buffer,
-        medium: medium_buffer,
-        big: big_buffer,
     }
 }
